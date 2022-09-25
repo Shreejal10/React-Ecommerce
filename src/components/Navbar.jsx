8d0250473efa51cfb6b32eaac4087551144250f0
Shreejal Dhungana <dshreejal@gmail.com>
2022-09-25 19:47:19 +0545

    added cart count

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,10 @@ const Navbar = () => {
             <div className='flex justify-between items-center p-4 z-[100] w-full absolute bg-[#081621]'>
                 <h1 className='text-white text-3xl font-bold cursor-pointer'>Ecommerce</h1>
                 <div className='flex'>
-                    <button className=' px-4 py-2 cursor-pointer text-white  pl-4 flex  gap-1 items-center justify-center hover:text-[#A167A5] hover:scale-110 active:text-[#A167A5]'> <AiOutlineShoppingCart /> Cart</button>
+                    <button className=' px-4 py-2 cursor-pointer text-white  pl-4 flex  gap-1 items-center justify-center hover:text-[#A167A5] hover:scale-110 active:text-[#A167A5] '> <AiOutlineShoppingCart /> Cart
+                        <span className='text-white relative bottom-3 bg-red-400 rounded-lg px-1 py text-sm'>0</span>
+                    </button>
+
                     <button className=' px-4 py-2 cursor-pointer text-white flex items-center gap-1 justify-center hover:text-[#A167A5] hover:scale-110 active:text-[#A167A5]'> <VscSignIn /> Sign In</button>
                 </div>
             </div>
