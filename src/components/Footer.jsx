b8e95f47bc47b50c1f1a6f475c020dfc4b7f2dcd
Shreejal Dhungana <dshreejal@gmail.com>
2022-10-13 18:35:55 +0545

    style changes footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='text-center text-gray-400 text-base h-[5vh]'>&copy; 2022  Shreejal Dhungana</div>
+        <div className='text-center text-gray-600 text-base h-[5vh] flex items-center justify-center'>&copy; 2022  Shreejal Dhungana</div>
     )
 }
 
