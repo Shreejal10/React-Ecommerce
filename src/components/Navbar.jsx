b516068fb96e96ec19eca23a75539c0930d993ad
Shreejal Dhungana <dshreejal@gmail.com>
2022-11-29 13:29:05 +0545

    display cart count in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { VscSignIn, VscSignOut } from 'react-icons/vsc'
 import logo from '../assets/logo.png'
 import { NavLink, Link } from 'react-router-dom'
 import { auth } from '../firebase/config'
 import { useAuthState } from 'react-firebase-hooks/auth'
-
+import { collection, getCountFromServer } from "firebase/firestore";
+import { db, } from "../firebase/config"
 const Navbar = () => {
+
+    const [count, setCount] = useState(0);
+    let a
+    const uid = localStorage.getItem('uid')
+    const getCount = async () => {
+        const colRef = collection(db, `${"cart-"}${uid}`);
+        const snapshot = await getCountFromServer(colRef)
+        setCount(snapshot.data().count)
+        a = count
+    }
+
+    useEffect(() => {
+        getCount()
+    }, [])
+
     const [user] = useAuthState(auth);
     return (
         <div className='flex justify-between items-center p-4 z-[100] w-full bg-[#081621] h-[10vh]'>
@@ -17,7 +33,7 @@ const Navbar = () => {
                 {user ?
                     <NavLink to='/cart' className='px-1 py-2 cursor-pointer text-white  pl-4 flex  gap-1 items-center justify-center  hover:scale-110 '>
                         <AiOutlineShoppingCart /> Cart
-                        <span className='text-white relative bottom-3 bg-red-400 rounded-lg px-1 py text-sm'>0</span>
+                        <span className='text-white relative bottom-3 bg-red-400 rounded-lg px-1 py text-sm'>{count}</span>
                     </NavLink>
                     : <Link to='/signin' className='px-1 py-2 cursor-pointer text-white  pl-4 flex  gap-1 items-center justify-center  hover:scale-110 '>
                         <AiOutlineShoppingCart /> Cart
@@ -26,9 +42,6 @@ const Navbar = () => {
                 <button to='/signin' className=' px-4 py-2 cursor-pointer text-white flex items-center gap-1 justify-center  hover:scale-110'>
                     {user ? <Link to="/" onClick={() => auth.signOut(auth)} className='flex items-center justify-center gap-1 hover:text-[#A167A5]'><VscSignOut />Sign Out</Link> : <NavLink to='/signin' className='flex items-center justify-center gap-1'><VscSignIn />Sign In</NavLink>}
                 </button>
-                {/* <div>
-                    {user ? <NavLink to='/signin' className=' px-4 py-2 cursor-pointer text-white flex items-center gap-1 justify-center  hover:scale-110 '> </NavLink> : <NavLink to='/signin' className=' px-4 py-2 cursor-pointer text-white flex items-center gap-1 justify-center  hover:scale-110 '><VscSignIn />Sign In </NavLink>}
-                </div> */}
             </div>
         </div>
     )
